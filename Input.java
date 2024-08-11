fb648f3fe6a87b6192cd2bf450e1e98e91658bb9
Andres Hernandez <jorge.hez2005@gmail.com>
2024-08-11 10:08:14 -0500

    Revision datos entrada - condiciones documento

--- Input.java
+++ Input.java
@@ -20,18 +20,41 @@ public class Input {
     // Leer el archivo
     try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
       int numeroRutas = Integer.parseInt(br.readLine().trim());
+      if(!(numeroRutas >= 1 && numeroRutas <= 10000)){
+        System.out.println("El sistema de transporte publico de Nueva Konigsberg no puede tener más de 10000 rutas");
+        System.exit(1);
+      }
       for (int i = 0; i < numeroRutas; i++) {
         String[] partesRuta = br.readLine().split(" ");
-        int idRuta = Integer.parseInt(partesRuta[0]);
-        if(idRuta < 0){
-          continue;
+        if(datosCorrectos(partesRuta)) {
+          rutas.add(partesRuta);
         }
-        rutas.add(partesRuta);
       }
     } catch (IOException e) {
       System.out.println("El archivo indicado no se ha encontrado.");
       System.exit(0);
     }
     return rutas;
+  }
+
+  private static boolean datosCorrectos(String[] partesRuta){
+    int idRuta = Integer.parseInt(partesRuta[0]);
+    System.out.println(partesRuta.length);
+    if(idRuta < 0){
+//      System.out.println("RUTA #" + idRuta + " id invalido");
+      return false;
+      //identifica cuando una ruta tiene mas de 1000 paraderos
+    }if(partesRuta.length > 2000){
+//      System.out.println("RUTA #" + idRuta + " muchos paraderos");
+      return false;
+    }
+    //recorrer
+    for (int i = 2; i < partesRuta.length; i = i + 2) {
+      if(Integer.parseInt(partesRuta[i]) > 1000){
+//        System.out.println("RUTA #" + idRuta + " tiempo de viaje muy grande");
+        return false;
+      }
+    }
+    return true;
   }
 }
